e0172f7f6d1cbabfd473922479b681036a954076
lordsayur <umar_317@hotmail.com>
2022-05-30 07:09:24 +0800

    ✨ iteration: allow custom repeat count

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
