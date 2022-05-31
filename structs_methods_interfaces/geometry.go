2bb2ea13ef31e6fe0ce08130bc7edbc689b69577
lordsayur <umar_317@hotmail.com>
2022-05-31 13:53:44 +0800

    ✨ geometry: add Triangle

--- structs_methods_interfaces/geometry.go
+++ structs_methods_interfaces/geometry.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
