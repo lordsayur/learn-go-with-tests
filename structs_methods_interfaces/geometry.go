d0bebcd65651f44742ab0cc34a926b691f86e68f
lordsayur <umar_317@hotmail.com>
2022-05-31 13:58:15 +0800

    ♻️ geometry: fix Rectangle property name

--- structs_methods_interfaces/geometry.go
+++ structs_methods_interfaces/geometry.go
@@ -7,12 +7,12 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 func (r Rectangle) Area() float64 {
-	return r.width * r.height
+	return r.Width * r.Height
 }
 
 type Circle struct {
@@ -33,5 +33,5 @@ type Triangle struct {
 }
 
 func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+	return 2 * (rectangle.Width + rectangle.Height)
 }
--- structs_methods_interfaces/geometry_test.go
+++ structs_methods_interfaces/geometry_test.go
@@ -19,7 +19,7 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{width: 12, height: 6}, want: 72.0},
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
 		{shape: Circle{Radius: 10}, want: 314.1592653589793},
 		{shape: Triangle{Base: 12, Height: 6}, want: 36},
 	}
