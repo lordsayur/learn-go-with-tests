bdeb061725f59870dc23b7bdd9add2272d0b64bf
lordsayur <umar_317@hotmail.com>
2022-05-31 15:25:51 +0800

    ✨ fintech: handle errcheck

--- pointers_and_errors/fintech_test.go
+++ pointers_and_errors/fintech_test.go
@@ -12,7 +12,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		error := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, error)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -32,6 +34,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
