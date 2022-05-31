a46af30df152701fa1091f37b20d727a5d4fa3d8
lordsayur <umar_317@hotmail.com>
2022-05-31 11:35:30 +0800

    ✨ geometry: add Circle

--- structs_methods_interfaces/geometry.go
+++ structs_methods_interfaces/geometry.go
@@ -1,8 +1,22 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
