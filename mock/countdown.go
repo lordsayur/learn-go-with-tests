c1dea33d543db6f3f120488028964865d8157c3c
lordsayur <umar_317@hotmail.com>
2022-06-18 10:39:49 +0800

    ✨ mock: add Configurable sleep

--- mock/countdown.go
+++ mock/countdown.go
@@ -33,6 +33,23 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 const write = "Write"
 const sleep = "sleep"
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
